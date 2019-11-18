a9873e5736a7f83ecd94cb1cbbc71daabdbcf1e8
谢在奎 <jackshea@163.com>
2019-11-18 21:46:08 +0800

    引入Nlog

--- Assets/Scripts/SceneFlow/StartupScene.cs
+++ Assets/Scripts/SceneFlow/StartupScene.cs
@@ -1,7 +1,19 @@
-﻿namespace SceneFlow
+﻿using NLog;
+
+namespace SceneFlow
 {
     public class StartupScene : BaseScene
     {
-
+        static readonly Logger _log = LoggerFactory.GetLogger(typeof(StartupScene).Name);
+        public override void KernelLoaded()
+        {
+            base.KernelLoaded();
+            _log.Trace("test nlog trace");
+            _log.Debug("test nlog debug");
+            _log.Info("test nlog info");
+            _log.Warn("test nlog warning");
+            _log.Error("test nlog error");
+            _log.Fatal("test nlog fatal");
+        }
     }
 }
