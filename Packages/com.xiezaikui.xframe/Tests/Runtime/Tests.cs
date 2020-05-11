3956de3503339ada52994e910582fe995778a7ab
谢在奎 <jackshea@163.com>
2020-05-11 23:11:47 +0800

    添加测试框架

---
+++ Packages/com.xiezaikui.xframe/Tests/Runtime/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
